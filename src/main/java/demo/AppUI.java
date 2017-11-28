5cb2fd62742c03be6cbd7c8e6503c085b13a05d6
Yuriy Artamonov <artamonov@haulmont.com>
2017-11-28 19:57:51 +0400

    Enable WebSocket

--- src/main/java/demo/AppUI.java
+++ src/main/java/demo/AppUI.java
@@ -1,10 +1,13 @@
 package demo;
 
+import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.shared.ui.ui.Transport;
 import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
 
+@Push(transport = Transport.WEBSOCKET)
 @Theme(ValoTheme.THEME_NAME)
 public class AppUI extends UI {
     @Override
